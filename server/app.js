6c86ae99f840571766812c71ddd1c3abac6e76ea
Aya Qunoo <qunooa@gmail.com>
2023-06-08 13:56:13 +0200

    create express server

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.set('port', process.env.PORT || 5000);
+
+app.disable('x-powered-by');
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+module.exports = app;
