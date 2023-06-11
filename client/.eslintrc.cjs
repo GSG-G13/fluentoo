f452dbb3090f26ebc3de26f79a3862f13112a65a
Aya Qunoo <qunooa@gmail.com>
2023-06-11 13:40:49 +0200

    feat: initilaization react with vit

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -10,5 +10,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
+    'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
+
   },
 }
