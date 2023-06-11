79062e546ffb7d5bd0282a0128df34cc6b94fd74
Aya Qunoo <qunooa@gmail.com>
2023-06-11 15:51:55 +0200

    fix: env setup

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -1,14 +1,21 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const url = process.env.DB_URL;
+const { NODE_ENV } = process.env;
+
+const url = NODE_ENV === 'development' ? process.env.DB_URL : process.env.DB_URL_production;
 
 const sequelize = new Sequelize(url, {
   logging: true,
   dialectOptions: {
     charset: 'utf8',
   },
+  ssl: {
+    require: true,
+    rejectUnauthorized: NODE_ENV !== 'development',
+  },
 });
+
 sequelize.sync();
 
 module.exports = sequelize;
