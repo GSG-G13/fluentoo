90918e8d870d3154bf79fc0ccde316f009dc1645
MohammedMahani <mohammed.mahanu@gmail.com>
2023-06-12 00:51:47 +0300

    fix: addlanguage controller

--- server/controllers/language/addLanguage.js
+++ server/controllers/language/addLanguage.js
@@ -1,13 +1,16 @@
 const Language = require('../../models/language');
 
-const createLanguage = (req, res) => {
-  const { name, shortcut, flag } = req.body;
-  Language.create({ name, shortcut, flag })
-    .then((data) => res.status(200).json({
+const createLanguage = async (req, res) => {
+  try {
+    const { name, shortcut, flag } = req.body;
+    const data = await Language.create({ name, shortcut, flag });
+    res.status(200).json({
       message: 'language created successfully',
       data,
-    }))
-    .catch((err) => res.status(500).send(`Server error: ${err}`));
+    });
+  } catch (err) {
+    res.status(500).send(`Server error: ${err}`);
+  }
 };
 
 module.exports = createLanguage;
