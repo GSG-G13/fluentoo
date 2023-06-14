5e4e559bdcb3b3ad62fac576ca3fb34976f6914c
Aya Qunoo <qunooa@gmail.com>
2023-06-14 15:07:26 +0200

    fix: tests

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -10,10 +10,10 @@ const sequelize = new Sequelize(url, {
   dialectOptions: {
     charset: 'utf8',
   },
-  ssl: {
-    require: true,
-    rejectUnauthorized: NODE_ENV !== 'development',
-  },
+  // ssl: {
+  //   require: true,
+  //   rejectUnauthorized: NODE_ENV !== 'development',
+  // },
 });
 if (NODE_ENV === 'development') {
   // sequelize.sync();
