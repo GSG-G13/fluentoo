77168a827d4085e6da8f27bfd3f0664efb89f4de
Abdullah Alsharif <adalah.sh00@gmail.com>
2023-06-20 13:56:40 +0300

    fix: changing as requested

--- server/models/message.js
+++ server/models/message.js
@@ -4,22 +4,6 @@ const sequelize = require('../database/config/connection');
 const User = require('./user');
 
 const Message = sequelize.define('messages', {
-  sender: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: User,
-      key: 'id',
-    },
-  },
-  receiver: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    references: {
-      model: User,
-      key: 'id',
-    },
-  },
   content: {
     type: DataTypes.TEXT,
     allowNull: false,
