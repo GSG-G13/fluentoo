a73bfa15e0753212e431c4a1f3417c14e66a14c1
Abdullah Alsharif <adalah.sh00@gmail.com>
2023-06-23 03:46:58 +0300

    fix: index of server more clean

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-console */
 const app = require('./app');
+const { socketInit } = require('./socket');
 
 const server = app.listen(app.get('port'), () => {
   console.log(`server is listening on port ${app.get('port')},${process.env.NODE_ENV} mode`);
 });
 
-module.exports = server;
-require('./socket');
+socketInit(server);
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,51 +1,52 @@
 /* eslint-disable no-param-reassign */
 const cookie = require('cookie');
 const { WebSocketServer } = require('ws');
-const server = require('..');
 const { verfiyToken, CustomeError } = require('../utils');
 
-const wss = new WebSocketServer({ server });
-
-const assignClient = async (connection, req) => {
-  try {
-    const cookies = req.headers.cookie;
-    const { token } = cookie.parse(cookies);
-    const decoded = await verfiyToken(token);
-    connection.userId = decoded.id;
-    connection.userName = decoded.username;
-  } catch (err) {
-    throw new CustomeError('Please login first', 401);
-  }
-};
-
-const getOnlineUsers = () => {
-  const onlineUsers = [...wss.clients]
-    .map((client) => ({ userId: client.userId, userName: client.userName }));
-
-  wss.clients.forEach((client) => {
-    client.send(JSON.stringify({ onlineUsers }));
-  });
-};
-
-const sendMessage = (receivedMessage) => {
-  const { text, recipient } = receivedMessage.message;
-  wss.clients.forEach((client) => {
-    if (client.userId === recipient) {
-      client.send(JSON.stringify({ text }));
+const init = (server) => {
+  const wss = new WebSocketServer({ server });
+  const assignClient = async (connection, req) => {
+    try {
+      const cookies = req.headers.cookie;
+      const { token } = cookie.parse(cookies);
+      const decoded = await verfiyToken(token);
+      connection.userId = decoded.id;
+      connection.userName = decoded.username;
+    } catch (err) {
+      throw new CustomeError('Please login first', 401);
     }
+  };
+
+  const getOnlineUsers = () => {
+    const onlineUsers = [...wss.clients]
+      .map((client) => ({ userId: client.userId, userName: client.userName }));
+
+    wss.clients.forEach((client) => {
+      client.send(JSON.stringify({ onlineUsers }));
+    });
+  };
+
+  const sendMessage = (receivedMessage) => {
+    const { text, recipient } = receivedMessage.message;
+    wss.clients.forEach((client) => {
+      if (client.userId === recipient) {
+        client.send(JSON.stringify({ text }));
+      }
+    });
+  };
+  wss.on('connection', async (connection, req) => {
+    await assignClient(connection, req);
+
+    getOnlineUsers();
+
+    connection.on('message', (message) => {
+      const receivedMessage = JSON.parse(message.toString());
+
+      if ('message' in receivedMessage) {
+        sendMessage(receivedMessage);
+      }
+    });
   });
 };
 
-wss.on(('connection'), async (connection, req) => {
-  await assignClient(connection, req);
-
-  getOnlineUsers();
-
-  connection.on('message', (message) => {
-    const receivedMessage = JSON.parse(message.toString());
-
-    if ('message' in receivedMessage) {
-      sendMessage(receivedMessage);
-    }
-  });
-});
+module.exports = { socketInit: init };
