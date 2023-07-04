8962badcf7b9a1d500ffdfecf69be314e91fd3f4
Aya Qunoo <qunooa@gmail.com>
2023-07-04 19:13:13 +0200

    build: pagination frontend

--- client/src/components/Card/index.tsx
+++ client/src/components/Card/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Card, Rate, Button, Image } from 'antd';
+
 import {
   SendOutlined
 } from '@ant-design/icons';
 import './style.modules.css'
+import Flag from 'react-world-flags'
 function UserCard(props: any) {
   let data = props.data;
-  console.log(data);
+ console.log(data);
+ 
 
   return (
     <Card className='card' >
@@ -27,8 +30,8 @@ function UserCard(props: any) {
       <div className='bottom'>
         <Rate disabled defaultValue={0} value={data.avgRating} />
         <div className='flags'>
-          <img src="https://cdn-icons-png.flaticon.com/128/206/206657.png" alt="" />
-          <img src="https://cdn-icons-png.flaticon.com/128/555/555613.png" alt="" /></div>
+          <Flag code={data.profile?.country} />
+        </div>
       </div>
 
 
--- client/src/pages/Community/index.tsx
+++ client/src/pages/Community/index.tsx
@@ -1,22 +1,34 @@
 import React, { useEffect, useState } from 'react'
-import { Row, Input } from 'antd';
+import { Input } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 import { Menu, UserCard } from '../../components';
+import { Pagination } from 'antd';
 import axios from 'axios';
 import './style.modules.css'
-
-
+import { Empty } from 'antd';
 function Community() {
   const [name, setName] = useState<string>('')
   const [spokenLanguages, setSpokenLanguages] = useState<string>('')
   const [practiceLanguages, setPracticeLanguages] = useState<string>('')
   const [data, setData] = useState<Array<Object>>([])
+  const [currentPage, setCurrentPage] = useState(1);
+  const [usersPerPage] = useState(8)
+  const indexOfLastUser = currentPage * usersPerPage;
+  const indexOfFirstUser = indexOfLastUser - usersPerPage;
+  const currentPosts = data.slice(indexOfFirstUser, indexOfLastUser);
 
+
+
+
+  const onchange = (page: any) => {
+    setCurrentPage(page);
+  };
   useEffect(() => {
     const fetchData = async () => {
       const response =
         await axios.get(`/api/v1/search?name=${name}&spokenLanguages=${spokenLanguages}&practiceLanguages=${practiceLanguages}`);
       setData(response.data.data)
+      setCurrentPage(1)
     }
     fetchData();
   }, [name, spokenLanguages, practiceLanguages])
@@ -30,13 +42,17 @@ function Community() {
           <Menu name={'native language'} setLanguage={setSpokenLanguages} />
           <Menu name={'practice language'} setLanguage={setPracticeLanguages} />
         </div>
-
       </div>
       <div className='community-cards'>
-        {data.map((user) =>
-          <UserCard data={user} />
-        )}
+        {currentPosts.length ? currentPosts.map((user, index) =>
+          <UserCard data={user} key={index} />
+        ) : <Empty className='not-found' description={'user not found'} />}
+
       </div>
+      <div className='pagination'>
+        <Pagination defaultCurrent={currentPage} defaultPageSize={usersPerPage} total={data.length} onChange={onchange} />
+      </div>
+
     </div>
   )
 }
