bf667c2add7efc3760e34ddcf7025bfdad203bf2
stmarouf1998 <stmarouf1998@gmail.com>
2023-07-10 16:22:26 +0300

    fix: tests

--- server/config/mail.config.js
+++ server/config/mail.config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const { GOOGLE_USER, PASSWORD, PORT_STMP } = process.env;
 
 module.exports = {
-  user: GOOGLE_USER,
-  password: PASSWORD,
-  port: PORT_STMP,
+  user: GOOGLE_USER || 'fluentoedu@gmail.com',
+  password: PASSWORD || 'evmqlkphqbndkjqs',
+  port: PORT_STMP || 587,
 };
