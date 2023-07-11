6c7a3e2d4eb662baaa76c6e52dba10be829fcba0
Aya Qunoo <qunooa@gmail.com>
2023-07-11 02:06:04 +0200

    build: get languages and handle them in the front

--- server/controllers/language/getAllLanguages.js
+++ server/controllers/language/getAllLanguages.js
@@ -2,7 +2,9 @@ const { Language } = require('../../models');
 
 const getAllLanguages = async (req, res, next) => {
   try {
-    const languages = await Language.findAll();
+    const languages = await Language.findAll({
+      attributes: ['name'],
+    });
     res.json({
       msg: 'Languages Returned Successfully',
       status: 200,
